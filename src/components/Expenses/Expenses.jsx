27609a26aac529cfa224c2a505df4f5703e86d64
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-01-29 18:22:42 +0100

    Make year filter work

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -7,6 +7,12 @@ import { useState } from "react";
 const Expenses = (props) => {
   const [expensesYear, setExpensesYear] = useState("2022");
 
+  const getFilteredExpenses = () => {
+    return props.items.filter(
+      (item) => item.date.getFullYear() == expensesYear
+    );
+  };
+
   const expensesYearChangeHandler = (newExpensesYear) => {
     setExpensesYear(newExpensesYear);
   };
@@ -17,26 +23,14 @@ const Expenses = (props) => {
         currentFilteredYear={expensesYear}
         onFilteredYearChange={expensesYearChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {getFilteredExpenses().map((item) => (
+        <ExpenseItem
+          key={item.id}
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        />
+      ))}
     </Card>
   );
 };
